30fb434af3264864a2a06ab96246b55a6f959df7
Vasilis-Palaskas <67687246+Vasilis-Palaskas@users.noreply.github.com>
2023-04-24 22:08:00 +0300

    Update

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,6 +2,8 @@
 
 # Below, we import libraries depending on each functionality/task we will implement in the next
 #---Data processing and some useful libraries
+
+# %%
 import requests
 import os
 import warnings
@@ -19,18 +21,20 @@ from functools import partial
 import tkinter as tk
 from  tkinter  import filedialog# library for choosing manually working directory
 from sklearn.impute import  KNNImputer, SimpleImputer
-#---Visualisation
+
+ # %% Visualisation
 import matplotlib.pyplot as plt
 import seaborn as sn 
 
 #---Machine-Deep learning Algorithms 
+# %%
 import sklearn
 from sklearn.preprocessing import LabelBinarizer, OneHotEncoder,OrdinalEncoder,StandardScaler, Normalizer, normalize,MinMaxScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 
-
+# %%
 import tensorflow as tf #pip install tensorflow
 from tensorflow import keras
 from tensorflow.keras.layers import Dense
@@ -38,16 +42,17 @@ from tensorflow.keras.layers import Dropout
 from tensorflow.keras.models import Sequential
 from scikeras.wrappers import KerasClassifier
 
+
 #--- Cross Validation-Model Selection scores-Pipelines Implementation-Hyperparameters Search
+# %%
+
 from sklearn.model_selection import cross_val_predict,cross_val_score, StratifiedKFold,GridSearchCV
 from sklearn.metrics import roc_auc_score, confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
 from BorutaShap import BorutaShap, load_data
 from sklearn import metrics  #Additional scklearn functions
 from xgboost import cv
-
 from sklearn.pipeline import Pipeline
 from sklearn.pipeline import make_pipeline
-
 import optuna## Optuna  Search method
 from skopt import BayesSearchCV
 from skopt.callbacks import DeadlineStopper, DeltaYStopper
@@ -55,11 +60,15 @@ from skopt.space import Real, Categorical, Integer
 
 
 #-- -*- coding: utf-8 -*-
+# %%
 import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
 # %%
-    """Now its time to load the data of this project
+    """
+    
+    Now its time to load the data of this project
+  
     """
